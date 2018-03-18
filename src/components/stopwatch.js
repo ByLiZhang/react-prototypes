fe18e633baf4006f9a0ac6a152abcc32e0198647
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-03-18 16:54:27 -0700

    feature2 done

---
+++ src/components/stopwatch.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Stopwatch extends Component {
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return (
+			<div>
+				<h3>This will be the stopwatch</h3>
+			</div>
+		);
+	}
+}
+
+export default Stopwatch;
