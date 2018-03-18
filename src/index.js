835f98e272b0042bfbfb63fde0896228a4b626c7
Li Zhang <bylizhang@gmail.com>
2018-03-17 19:42:42 -0700

    prototype complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+function luckyNumber() {
+	return Math.ceil(Math.random()*1000);
+}
+const user = {
+	name: 'Ukyo',
+	luckyNumber: luckyNumber()
+}
+function greeting(user) {
+	const element = (
+		<div className='container'>
+			<h1>Welcome {user.name}</h1>
+			<h2 className='text-muted'>You lucky number is {user.luckyNumber}</h2>
+		</div>
+	);
+	return element;
+}
 
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
