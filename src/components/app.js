1e3630ec50de0634dd9a902e45b47e15e369b6c7
Li Zhang <bylizhang@gmail.com>
2018-03-22 16:41:33 -0700

    feature 1&2 done

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,14 @@
+import React from 'react';
+import ContactList from './contact_list';
 
+const App = () => {
+	return (
+		<div className='container'>
+			<h1 className='text-center my-2'>Address Book</h1>
+			<ContactList/>
+		</div>
+	)
+}
+
+export default App;
 
---
+++ src/data/contacts.js
@@ -0,0 +1,20 @@
+export default [
+	{
+		firstName: "Andy",
+		lastName: "Anderson",
+		phone: "111-111-1111",
+		email: "1@2.com"
+	}, 
+	{
+		firstName: "Bobby",
+		lastName: "Branson",
+		phone: "222-222-2222",
+		email: "3@4.com"
+	}, 
+	{
+		firstName: "Charlie",
+		lastName: "Churchill",
+		phone: "333-333-3333",
+		email: "5@6.com"
+	},
+];
