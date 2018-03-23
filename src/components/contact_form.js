bf90abe1dc1e57b30a1d5c4f6dc05c493faed9ec
Li Zhang <bylizhang@gmail.com>
2018-03-22 22:08:56 -0700

    feature 5 done

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Field from './field';
 
 class ContactForm extends Component {
 	constructor(props){
@@ -6,7 +7,9 @@ class ContactForm extends Component {
 		this.state = {
 			form: {
 				firstName: '',
-				lastName: ''
+				lastName: '',
+				phone: '',
+				email: ''
 			}
 		}
 		this.handleInputChange = this.handleInputChange.bind(this);
@@ -22,22 +25,19 @@ class ContactForm extends Component {
 
 	handleSubmit(event){
 		event.preventDefault();
-		console.log(this.state.form);
+		console.log('handleSubmit called',this.state.form);
 	}
 
 	render(){
-		const { firstName, lastName } = this.state.form;
+		const { firstName, lastName, phone, email } = this.state.form;
 
 		return (
 			<form onSubmit={this.handleSubmit}>
-				<div className='form-group'>
-					<label>First Name</label>
-					<input name='firstName' type='text' className='form-control' value={firstName} onChange={this.handleInputChange}/>
-				</div>
-				<div className='form-group'>
-					<label>Last Name</label>
-					<input name='lastName' type='text' className='form-control' value={lastName} onChange={this.handleInputChange}/>
-				</div>
+				<Field name='firstName' label='First Name' type='text' value={firstName} onChange={this.handleInputChange}/>
+				<Field name='lastName' label='Last Name' type='text' value={lastName} onChange={this.handleInputChange}/>
+				<Field name='phone' label='Phone' type='text' value={phone} onChange={this.handleInputChange}/>
+				<Field name='email' label='Email' type='text' value={email} onChange={this.handleInputChange}/>
+				
 				<button>Add Contact</button>
 			</form>
 		)
