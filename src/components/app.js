06407ef9911c8ff81e125c047b85f032bfc4a2c8
Li Zhang <bylizhang@gmail.com>
2018-03-22 22:56:20 -0700

    feature 6 done

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React,{ Component }  from 'react';
 import ContactList from './contact_list';
 import ContactForm from './contact_form';
+import contactData from '../data/contacts';
 
+class App extends Component {
+	constructor(props){
+		super(props);
 
-const App = () => {
-	return (
-		<div className='container'>
-			<h1 className='text-center my-2'>Address Book</h1>
-			<div className='row'>
-				<div className='col-4'>
-					<ContactForm/>
+		this.state = {
+			contacts: contactData
+		};
+		this.addContact = this.addContact.bind(this);
+	}
+
+	addContact(contact){
+		console.log('App: addComponent', contact);
+		this.setState({
+			contacts: [...this.state.contacts, contact] //check [] usage
+		});
+	}
+
+	render(){
+		return (
+			<div className='container'>
+				<h1 className='text-center my-2'>Address Book</h1>
+				<div className='row'>
+					<div className='col-4'>
+						<ContactForm add={this.addContact}/>
+					</div>
+					<ContactList contacts={this.state.contacts}/>
 				</div>
-				<ContactList/>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default App;
--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -26,6 +26,7 @@ class ContactForm extends Component {
 	handleSubmit(event){
 		event.preventDefault();
 		console.log('handleSubmit called',this.state.form);
+		this.props.add(this.state.form);
 	}
 
 	render(){
