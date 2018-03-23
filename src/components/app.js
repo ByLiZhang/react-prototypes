9565319449af203ce2c63cbb55cf8c00b81d3b8f
Li Zhang <bylizhang@gmail.com>
2018-03-22 21:16:22 -0700

    feature 4 done

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ContactList from './contact_list';
+import ContactForm from './contact_form';
+
 
 const App = () => {
 	return (
 		<div className='container'>
 			<h1 className='text-center my-2'>Address Book</h1>
-			<div className='row'><ContactList/></div>
+			<div className='row'>
+				<div className='col-4'>
+					<ContactForm/>
+				</div>
+				<ContactList/>
+			</div>
 		</div>
 	)
 }
