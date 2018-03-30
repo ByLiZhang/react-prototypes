e591468e84c7c4a345b72bf7428f996c140f06e7
Li Zhang <bylizhang@gmail.com>
2018-03-29 17:46:01 -0700

    feature 1 and 2 done

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,15 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import Welcome from './welcome';
 
+const App = () => {
+	return(
+		<div className='container'>
+			<h1>test</h1>
+			<Route exact path = "/" component = {Welcome} />
+		</div>
+	);
+}
+
+export default App;
 
---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => {
+	return (
+		<div>
+			<h1>No trespassing!</h1>
+			<p>lorem ipsum</p>
+		</div>
+	);
+}
