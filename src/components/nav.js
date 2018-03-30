b14345945553abc12140628891627e6f0a8dc549
Li Zhang <bylizhang@gmail.com>
2018-03-29 18:13:18 -0700

    feature 4 done

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,8 +4,9 @@ import {Link} from 'react-router-dom';
 export default props => {
 	return(
 		<ul className='nav nav-tabs mt-3'>
-			<li className='nav-item'>
+			<li className='nav-item row'>
 				<Link to='/' className='nav-link'>Welcome</Link>
+				<Link to='/our_macarons' className='nav-link'>Our Macarons</Link>
 			</li>
 		</ul>
 	);
---
+++ src/components/our_macarons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => {
+	return (
+	<div>
+		<h1>See our delicious macarons</h1>
+		<p>lorem ipsum</p>
+	</div>
+	);
+}
