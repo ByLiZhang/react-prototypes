c48b860c9195fb1943864241bfc73f36fe9a76c8
Li Zhang <bylizhang@gmail.com>
2018-04-03 10:19:14 -0700

    feature 1 done

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,12 @@
+import React from 'react';
 
+const App = () => {
+	return (
+		<div className='container'>
+			<h1 className='text-center'>Testing clock</h1>
+		</div>
+	);
 
+}
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { combineReducers } from 'redux';
+import clockReducer from './clock_reducer';
+
+export default combineReducers({clock: clockReducer});
