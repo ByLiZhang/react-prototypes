c2fdf61f02703106fb3a1954b2b1405edb461a0b
Li Zhang <bylizhang@gmail.com>
2018-04-03 10:30:57 -0700

    feature 2 done

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Clock from './clock';
 
 const App = () => {
 	return (
 		<div className='container'>
 			<h1 className='text-center'>Testing clock</h1>
+			<Clock/>
 		</div>
 	);
 
