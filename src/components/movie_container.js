63de9f31803db8a5d9053c5835baecc891e7cb31
Li Zhang <bylizhang@gmail.com>
2018-04-02 17:07:07 -0700

    feature 2 done

--- src/components/movie_container.js
+++ src/components/movie_container.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import Movie from './movies';
+import axios from 'axios';
 
 
 class MoviesContainer extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			movies: []
+		};
+	}
+
+	componentWillMount(){
+		const BASE_URL = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
+		axios.get(BASE_URL).then(resp =>{
+			console.log(resp);
+			this.setState({...this.state, movies: resp.data.feed.entry});
+		});
+	}
 
 	render(){
+		console.log(this.state);
 		return (
 			<div>
 				<h2>Movie Container</h2>
